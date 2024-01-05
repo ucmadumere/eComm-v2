fcd2e4257c135073a44366c18bb25f971a948481
ucmadumere <ucyweb@gmail.com>
2024-01-05 04:56:57 +0100

    shipping fee

--- clientside/src/utils/cartUtils.js
+++ clientside/src/utils/cartUtils.js
@@ -6,8 +6,8 @@ export const updateCart = (state) => {
      // Calc items price
      state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
 
-     // Calc shipping price (if order is over N100,000 then free, else N5000 shipping)
-     state.shippingPrice = addDecimals(state.itemsPrice > 100000 ? 0 : 5000);
+     // Calc shipping price (if order is over 2,000 then free, else N5000 shipping)
+     state.shippingPrice = addDecimals(state.itemsPrice > 2000 ? 0 : 50);
 
      // Calc tax price
      state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
